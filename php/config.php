db4510bc91faea283081604ac204cbaf6f206a89
Florian Dejonckheere <florian@floriandejonckheere.be>
2014-01-13 18:08:15 +0100

    Improved file listing

--- php/config.php
+++ php/config.php
@@ -1,4 +1,16 @@
 <?php
-define('ROOT_DIR',	'.');
+define('ROOT_DIR',	'/home/deluge/Torrents/');
+
+// White-list for allowed paths
+$w_list = array(
+	"/srv/http/main/bootstrap-fm",
+	"/home/deluge/Torrents/Finished",
+);
+
+/*
+ * DEBUG OPTIONS
+ * 
+ * */
+ini_set("display_errors", 1);
 
 ?>
--- php/list.php
+++ php/list.php
@@ -3,34 +3,46 @@
  * list.php - directory listing
  *
  * */
+ /*
+ * Error code	Description
+ * ----------	-----------
+ * 403		Directory not white-listed
+ * 404		File or directory not found
+ *
+ * */
 
 require_once('config.php');
 
 header('Content-Type: application/json');
 
-if(isset($_GET['dir']))
-{
-	$fDir = $_GET['dir'];
-} else {
-	$fDir = ROOT_DIR;
+$fDir = (isset($_GET['dir']) ? $_GET['dir'] : ROOT_DIR);
+$aPath = realpath((substr($fDir, 0, 1) == '/' ? $fDir : getcwd() . '/' . $fDir));
+$sContent = array('error'	=>	403);
+
+foreach($w_list as $w_dir){
+	if(strpos($aPath, $w_dir) !== 0)
+		unset($sContent['error']);
 }
 
-$iFiles = 0;
-$sContent = array(
-	"path"		=> $fDir,
-	"directories"	=> array(),
-	"files"		=> array(),
-);
-if($hDir = opendir($fDir))
+if(!isset($sContent['error']))
 {
-	while (false !== ($file = readdir($hDir)))
+	if(!($hDir = opendir($fDir)))
 	{
-		if($file != '.' && $file != '..')
+		$sContent['errors'] = 404;
+	} else {
+		$sContent['path'] = $aPath;
+		$sContent['directories'] = array();
+		$sContent['files'] = array();
+		
+		while (false !== ($file = readdir($hDir)))
 		{
-				array_push($sContent[(is_dir($file) ? 'directories' : 'files')], $file);
+			if($file != '.' && $file != '..')
+			{
+				array_push($sContent[(is_dir(realpath($fDir . '/' . $file)) ? 'directories' : 'files')], $file);
+			}
 		}
 	}
-} else die('Error opening directory \'' . $fDir . '\'');
+}
 
 echo json_encode($sContent);
 
